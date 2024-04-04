d580da12d6d4c89af95e08b254b5fc0b0a60fc1a
Pragya Kumari <pragyakumari121@gmail.com>
2024-04-04 15:26:52 +0530

    code updated

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,67 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.getName(),student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.getName(), teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
-            // your code goes here
+            // your code goes
+            List<String> studentList= new ArrayList<>();
+            if(teacherStudentMapping.containsKey(teacher)){
+                studentList=teacherStudentMapping.get(teacher);
+            }
+            studentList.add(student);
+            teacherStudentMapping.put(teacher,studentList);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        if(studentMap.containsKey(student)){
+            return studentMap.get(student);
+        }
+        return null;
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        if(teacherMap.containsKey(teacher)){
+            return teacherMap.get(teacher);
+        }
+        return null;
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        List<String> studentList=new ArrayList<>();
+        if(teacherStudentMapping.containsKey(teacher)){
+            studentList= teacherStudentMapping.get(teacher);
+        }
+        return studentList;
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> allStudentList = new ArrayList<>(studentMap.keySet());
+        return allStudentList;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        if(teacherMap.containsKey(teacher)){
+            teacherMap.remove(teacher);
+        }
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        teacherMap= new HashMap<>();
     }
 }
